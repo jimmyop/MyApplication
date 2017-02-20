572f742070c41cb26c72b67f4bf037a40e75feb6
jimmyop <413748190@qq.com>
2017-02-20 17:13:38 +0800

    base activity

---
+++ commonlibrary/src/main/java/com/jimmy/commonlibrary/base/BaseActivity.java
@@ -0,0 +1,29 @@
+package com.jimmy.commonlibrary.base;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by chenjiaming1 on 2017/2/20.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initView();
+        initData(savedInstanceState);
+        initListener();
+    }
+
+    protected abstract void initView();
+
+    protected abstract void initData(Bundle savedInstanceState);
+
+    protected abstract void initListener();
+
+
+
+}
